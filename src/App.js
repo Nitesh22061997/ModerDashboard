fc6d81c6e4b42aeadff762ff7d05028805a1d967
nitesh22061997@gmail.com <163007323+Nitesh22061997@users.noreply.github.com>
2024-08-03 16:49:40 +0530

    Topbar coompleted

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Route, Routes } from "react-router";
+import Topbar from "./scenes/global/Topbar";
+import Dashboard from "./scenes/dashboard";
+// import Charts from "./scenes/charts";
+// import Notes from "./scenes/notes";
+// import Wallet from "./scenes/wallet";
+// import Shop from "./scenes/shop";
+
 
 function App() {
+
+  const [theme, colorMode] = useMode()
   return (
-    <div>Dashboard</div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <div className="content">
+            <Topbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              {/* <Route path="/charts" element={<Charts />} /> */}
+              {/* <Route path="/notes" element={<Notes />} /> */}
+              {/* <Route path="/wallet" element={<Wallet />} /> */}
+              {/* <Route path="/shop" element={<Shop />} /> */}
+            </Routes>
+          </div>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
